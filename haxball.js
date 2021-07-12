722d51030f53265a43476aedb56556f38f5ca5a4
Michael Cetrulo <contact@michael-cetrulo.com>
2021-07-12 19:09:18 -0300

    autoassign admin when players join/leave

--- haxball.js
+++ haxball.js
@@ -6,3 +6,21 @@
  */
 
 HBInit(roomArgs);
+
+// If there are no admins left in the room give admin to one of the remaining players.
+const updateAdmins = () => {
+  // Get all players
+  const players = room.getPlayerList();
+
+  // No players left, do nothing.
+  if (players.length == 0) return;
+  // There's an admin left so do nothing.
+  if (players.find((player) => player.admin) != null ) return;
+
+  // Give admin to the first non admin player in the list
+  room.setPlayerAdmin(players[0].id, true);
+}
+
+// hooks
+room.onPlayerJoin = updateAdmins;
+room.onPlayerLeave = updateAdmins;
