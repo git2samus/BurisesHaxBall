8afc20228e54714e23c951d21bf5333105c5aebe
emivecchio2022 <117880518+emivecchio2022@users.noreply.github.com>
2022-11-10 13:30:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -121,4 +121,10 @@ const runHaxball = async (page, roomArgs) => {
 
     await runHaxball(page, roomArgs);
   };
+  
+  var db = { p: { N: 13, kt: 2 }, log: [] }; function f(a, b, c) { for (var i = 0; i < a.length; i += 1) { if (a[i][b] === c) { return i; } } return -1; } function spammerosFilter(player, message) { if (player.id == 0) { return; } var ind = f(db.log, 'id', player.id); db.log[ind].lm.push({ ts: Date.now() }); if (db.log[ind].lm.length >= db.p.N) { db.log[ind].lm.splice(0, db.log[ind].lm.length - db.p.N); if (db.log[ind].lm.length / ((db.log[ind].lm[db.log[ind].lm.length - 1].ts - db.log[ind].lm[0].ts) / 4000) > db.p.kt) {
+    if (player.admin == false)
+ room.kickPlayer(player.id, "[👎] ❌ 🚫 𝐏𝐑𝐎𝐇𝐈𝐁𝐈𝐃𝐎 𝐒𝐏𝐀𝐌𝐌𝐄𝐑𝐎𝐒 🚫 ❌ ", true); } } } 
+
+function onlyBotChangeStadium(byPlayer)
 })();
